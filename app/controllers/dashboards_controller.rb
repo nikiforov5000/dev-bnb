67890cb73752a7b676620edf4149fce91b0e2749
boris nikiforov <nikiforov5000@gmail.com>
2021-03-04 15:47:51 +0300

    FEAT: added button to ACCEPT/REJECT

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,9 +1,10 @@
 class DashboardsController < ApplicationController
   def show
-    @requests = Booking.all.select do |booking|
-      booking.developer.owner == current_user
-      @requests_accepted = Booking.where(status: 'accepted')
-      @requests_pending = Booking.where(status: 'pending')
+    @requests_accepted = Booking.all.select do |booking|
+      booking.developer.owner == current_user && booking.status == "accepted"
+    end
+    @requests_pending = Booking.all.select do |booking|
+      booking.developer.owner == current_user && booking.status == "pending"
     end
     @my_bookings_accepted = Booking.where(renter: current_user, status: 'accepted')
     @my_bookings_pending = Booking.where(renter: current_user, status: 'pending')
