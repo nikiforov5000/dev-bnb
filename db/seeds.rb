770ca34b4c2187e97bc5ca918ef30698a3e6f657
Zacharie Tournant ztournant@icloud.com <ztournant@icloud.com>
2021-03-04 11:47:08 +0300

    added skills to the skills table

--- db/seeds.rb
+++ db/seeds.rb
@@ -35,7 +35,7 @@ puts "Destroy Users"
       daily_rate: rand(20..200)
     )
     developer.save!
-    puts developer.owner 
+    puts developer.owner
     puts developer.name
     puts developer.specialty
     puts developer.age
@@ -50,10 +50,10 @@ end
 Booking.destroy_all
 20.times do
   booking = Booking.new(
-    project_name: Faker::Game.title, 
-    start_date: Date.new, 
-    end_date: Date.new + 1, 
-    total_price: rand(500..5000), 
+    project_name: Faker::Game.title,
+    start_date: Date.new,
+    end_date: Date.new + 1,
+    total_price: rand(500..5000),
     status: %w[accepted pending].sample,
     developer: Developer.all.sample,
     renter: User.all.sample
@@ -61,14 +61,14 @@ Booking.destroy_all
   booking.save!
   puts booking
 
-  
+
 end
 4.times do
   booking = Booking.new(
-    project_name: Faker::Game.title, 
-    start_date: Date.new, 
-    end_date: Date.new + 1, 
-    total_price: rand(500..5000), 
+    project_name: Faker::Game.title,
+    start_date: Date.new,
+    end_date: Date.new + 1,
+    total_price: rand(500..5000),
     status: %w[accepted pending].sample,
     developer: Developer.all.sample,
     renter: User.first
@@ -80,10 +80,10 @@ end
 end
 5.times do
   booking = Booking.new(
-    project_name: Faker::Game.title, 
-    start_date: Date.new, 
-    end_date: Date.new + 1, 
-    total_price: rand(500..5000), 
+    project_name: Faker::Game.title,
+    start_date: Date.new,
+    end_date: Date.new + 1,
+    total_price: rand(500..5000),
     status: %w[accepted pending].sample,
     developer: User.first.developers.sample,
     renter: User.all.sample
@@ -92,15 +92,15 @@ end
   puts booking
 end
 
-# REVIEW ############################ 
+# REVIEW ############################
 Review.destroy_all
 20.times do
   review = Review.new(
     title: ["good work", "excellent", "could be better", "finished on time AAA +++", "do not recommend"].sample,
     rating: rand(1..5),
     content: "Review sites can be a springboard for content
-    ideas – ideas that already reflect something your 
-    audience thinks, whether it’s about how to use your 
+    ideas – ideas that already reflect something your
+    audience thinks, whether it’s about how to use your
     product, what features matter, or pain points to overcome.",
     booking: Booking.all.sample
   )
@@ -110,6 +110,13 @@ Review.destroy_all
   puts review.content
 end
 
+Skill.destroy_all
+skills = %w[Backend Frontend SQL Rails UX UI Web-Design Git GitHub AJAX PHP JS Ruby C++ Python Matlab]
+skills.each do |skill|
+  add_skill = Skill.new(name: skill)
+  add_skill.save!
+  puts add_skill.name
+end
 puts "Finish"
 
 # Skill.destroy_all
