5b11a8d3e9e5ef5e0ea23b9c2238c2104e55fbc0
boris nikiforov <nikiforov5000@gmail.com>
2021-03-05 17:46:07 +0300

    FIX delete validation

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,7 +2,7 @@ class Review < ApplicationRecord
   belongs_to :booking
   has_one :developer, through: :booking
   has_one :user, through: :booking
-  validates :title, presence: true, length: { in: 3..16 }
+  validates :title, presence: true
   validates :content, presence: true, length: { in: 3..500 }
   validates :rating, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
 end
